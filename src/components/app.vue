4eee108e742131231515d3abe48ecaded8e7ccfb
tony <huangxiaodong@dcpai.cn>
2016-12-30 16:25:01 +0800

    add vue-webpack-sample ==> html scss

---
+++ src/components/app.vue
@@ -0,0 +1,37 @@
+<script>
+    export default {
+        el: "#app",
+        data() {
+            return {
+                name: "guowenfh",
+                age: "2000055555555555555555555000006"
+            }
+        }
+    }
+</script>
+<template>
+    <div>
+        <h1>姓名10：{{name}}</h1>
+        <h2>{{age}}</h2>
+        <h3>3333</h3>
+    </div>
+</template>
+<style>
+    $qwe:#098;
+    body {
+        background-color: $qwe;
+        h1{
+            background-color: #00f;
+            color: yellow;
+            transform: translate(10%, 10%);
+        }
+
+        h1:hover {
+            height: 100px;
+        }
+
+        h2 {
+            background-color: #888;
+        }
+    }
+</style>
